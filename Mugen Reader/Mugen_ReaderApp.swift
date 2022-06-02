f1b173968d4a5a7bf44d55f0edadb9027828e8d9
Carlos Mbendera <carlosmbendera@gmail.com>
2022-06-02 18:51:33 +0200

    Initial Commit

---
+++ Mugen Reader/Mugen_ReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mugen_ReaderApp.swift
+//  Mugen Reader
+//
+//  Created by Carlos Mbendera on 02/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct Mugen_ReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
